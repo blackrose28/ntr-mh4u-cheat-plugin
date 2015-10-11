3cc1fb8bf499a16ddd95a14263ca892556b2381c
Lê Văn Chương <levanchuong87@gmail.com>
2015-10-11 19:10:22 +0700

    Fix item base pointer, add charge blade support

--- README.md
+++ README.md
@@ -4,18 +4,24 @@ MH4U US cheat plugin for NTR CFW
 Tested with BootNTR.cia (NTR 3.0), N3DS XL emunand 9.0 Gateway, MH4U US cia version. Base on cell9's Zelda cheat plugin and base pointer of Seeker14491. Because Seeker base pointer is "not so base", so in some mission the plugin just doesn't work. In that case you have to try a difference mission.
 
 Features:
+Press SELECT + UP:
 - Set all items on first 3 pages of item pouch and gunner pouch. I ignore extra pouch because I just don't want crappy items add to storage each time I complete a hunt.
 - Display the first large monster's HP as first item's quantity in item pouch. Just set first item on lower screen item shortcut to view main monster's HP at all time during the hunt.
 - Set to max and freeze your stamina
 - Freeze your sharpness
+Press SELECT + R + UP:
 - Max long sword gauge after one hit
-- Make long sword glow red after first spirit-4 hit (Still, an unknown bug make the red gauge overflow)
+- Make long sword glow red after first spirit-4 hit (Still, the timer from the white glow make the red gauge overflow)
+Press SELECT + R + RIGHT:
+- Max C.Blade charge gauge
+- Set C.Blade number of phial to 6
+- Max C.Blade shield glow red
 
 Usage:
-Copy file cheat.plg to /plugin/0004000000126300/ in microsd card, boot to NTR and press SELECT during the hunt to enable cheats. Press SELECT again during hunt and the cheats is off. After a mission, the cheats also automatically off. Press SELECT anytime outside the hunt the plugin do nothing.
+Copy file cheat.plg to /plugin/0004000000126300/ in microsd card, boot to NTR and press the trigger button combination during the hunt to enable cheats. Press the trigger button combination again during hunt and the cheats is off. After a mission, the cheats also automatically off. Press the trigger button combination anytime outside the hunt the plugin do nothing.
 
 Download:
-https://mega.nz/#!JE43zBjY!rf3BU8Kk7Rsp6-J9MzLUEgDICIZRMqp1GbtkdlLuCYs
+https://mega.nz/#!dZB1zDzK!TGLIpYeYF2GDKGKASz-oTKS3OPVGbOFcHi6ix9tJHqU
 
 Credits:
 - NTR CFW by cell9 and companions
--- source/gameplg.c
+++ source/gameplg.c
@@ -10,7 +10,8 @@
 u32 threadStack[0x1000];
 Handle fsUserHandle;
 FS_archive sdmcArchive;
-u32 monsterHP;
+u16 monsterHP;
+u16 monster2HP;
 
 u32 plgGetIoBase(u32 IoType);
 #define IO_BASE_PAD		1
@@ -35,57 +36,27 @@ int checkPointerValid(pointer){
 }
 
 int checkStartOffsetValid(pointer){
-	return (pointer > 0x08920000) && (pointer < 0x08930000);
+	if ((pointer > 0x08920000) && (pointer < 0x08930000)){
+		u8 check = READU8(pointer - 0x1E0);
+		if (check != 0) return true;
+	}
+	return false;
 }
 
- // void itoa_reverse(u8 s[])
- // {
- //   int c, i, j;
- 
- //   for (i = 0, j = strlen(s)-1; i < j; i++ , j--)
- //   {
- //     c = s[i];
- //     s[i] = s[j];
- //     s[j] = c;
- //   }
- // }  
-
- // void my_itoa_2(u16 n, u8 s[])
- // {
- //   int i;
- 
- //   i = 0;
- //   do 
- //   {
- //     s[i++] = n % 10 + '0';
- //   }
- //   while ((n /= 10) >= 1);
- //   if ( i == 1)
- //     s[i++] = '0';
- //   s[i] = '\0'; 
- //   itoa_reverse(s);
- // }
- 
- 
- 
- //------------------------------ string reverse function -----------------------------//
- 
-
-
-// u32 showInfo(){
-// 	u8 str[15];
-// 	my_itoa_2(monsterHP,str);
-// 	showDbg("Monster HP:", 0, 0);
-// 	showDbg(str,0,0);
-// 	return 0;
-// }
-
 u32 cheatEnabled = 0;
+u32 longswordGaugeEnabled = 0;
+u32 chargeBladeGaugeEnabled = 0;
 int isNewNtr = 0;
 u8 longswordGauge = 0;
-u32 pointer0 = 0x081C7D00;
-u32 pointer1 = 0;
+u32 Mpointer0 = 0x081C7D00;
+u32 Mpointer1 = 0;
+u32 Mpointer2 = 0;
+u32 pointer0 = 0x081C7CD0;
+//u32 pointer0 = 0x081C739C;
+//u32 pointer0 = 0x081C7CD4;
+//u32 pointer1 = 0;
 u32 start_offset = 0;
+u32 itemOffset = 0;
 u16 Sharpness = 0;
 
 void gamePluginEntry() {
@@ -107,25 +78,44 @@ void gamePluginEntry() {
 	if (isNewNtr) {
 		IoBasePad = plgGetIoBase(IO_BASE_PAD);
 	}
-	monsterHP = 0;
+	//monsterHP = 0;
 	while (1) {
 		svc_sleepThread(100000000);
 		
 		key = getKey();
-		if (key == BUTTON_SE) {
-			// toggle cheats when SELECT button pressed
-			cheatEnabled = !cheatEnabled;
-			// wait until key is up
-			waitKeyUp();
+		switch(key){
+			case BUTTON_SE + BUTTON_DU:
+				cheatEnabled = !cheatEnabled;
+				break;
+			case BUTTON_SE + BUTTON_DU + BUTTON_R:
+				longswordGaugeEnabled = !longswordGaugeEnabled;
+				break;
+			case BUTTON_SE + BUTTON_DR + BUTTON_R:
+				chargeBladeGaugeEnabled = !chargeBladeGaugeEnabled;
+				break;
 		}
-		if (cheatEnabled) {
-			pointer1 = READU32(pointer0);
-			if (checkPointerValid(pointer1)){
-				start_offset = READU32(pointer1 + 0xE28) - 0x14030;
-				if (checkStartOffsetValid(start_offset)){
-					int i = 0;
+		waitKeyUp();
+		// if (key == BUTTON_SE) {
+		// 	// toggle cheats when SELECT button pressed
+		// 	cheatEnabled = !cheatEnabled;
+		// 	// wait until key is up
+		// 	waitKeyUp();
+		// }
+		if (cheatEnabled || longswordGaugeEnabled || chargeBladeGaugeEnabled){
+			start_offset = READU32(pointer0) + 0x1070;
+			if (checkStartOffsetValid(start_offset)){
+				if (cheatEnabled){
+					monsterHP = 0;
+					Mpointer1 = READU32(Mpointer0) + 0xE28;
+					if (checkPointerValid(Mpointer1)){
+						Mpointer2 = READU32(Mpointer1) + 0x3E8;
+						if (checkPointerValid(Mpointer2)){
+							monsterHP = READU16(Mpointer2);
+						}
+					}
 					//monsterHP
-					monsterHP = READU16(start_offset + 0x14418);
+					//monsterHP = READU16(start_offset + 0x14418);
+					//monster2HP = READU16(start_offset + 0x20AE8);
 					//Sharpness
 					Sharpness = READU16(start_offset + 0x94C6);
 					WRITEU16(start_offset + 0x94C4,Sharpness);
@@ -133,32 +123,50 @@ void gamePluginEntry() {
 
 					WRITEU16(start_offset + 0x11E,0x4461);
 					WRITEU16(start_offset + 0x122,0x4461);
-					//Long Sword
-					longswordGauge = READU8(start_offset + 0x134);
-					if (longswordGauge != 0){
-						WRITEU8(start_offset + 0x134,0x64);
-						WRITEU8(start_offset + 0x136,0x03);
-					}
+					
 					//Item
+					itemOffset = start_offset;
+					int i = 0;
 					for (i = 0;i < 32;i++){
 						if (i == 8){
-							if (monsterHP != 0){
-								WRITEU16(start_offset,monsterHP);	
+							if (monsterHP > 0){
+								WRITEU16(itemOffset,monsterHP);	
 							}else{
-								WRITEU8(start_offset,0x63);
+								WRITEU8(itemOffset,0x63);
 							}
 						}else{
-							WRITEU8(start_offset,0x63);
+							//if (i == 9){
+								// if (monster2HP > 0){
+								// 	WRITEU16(start_offset,monster2HP);
+								// }else{
+								//	WRITEU8(start_offset,0x63);
+								//}
+							//}else{
+								WRITEU8(itemOffset,0x63);
+							//}
 						}
-						start_offset = start_offset + 4;
+						itemOffset = itemOffset + 4;
 					}
-					
-				}else{
-					cheatEnabled = 0;
-				}	
+				}
+				if (longswordGaugeEnabled){
+					//Long Sword
+					// longswordGauge = READU8(start_offset + 0x134);
+					// if (longswordGauge != 0){
+						WRITEU8(start_offset + 0x134,0x64);
+						WRITEU8(start_offset + 0x136,0x03);
+					//}
+				}
+				if (chargeBladeGaugeEnabled){
+					WRITEU8(start_offset + 0x134,50);
+					WRITEU16(start_offset + 0x9292,0x4676);
+					WRITEU16(start_offset + 0x9744,0x0606);
+				}
+				
 			}else{
 				cheatEnabled = 0;
-			}
+				longswordGaugeEnabled = 0;
+				chargeBladeGaugeEnabled = 0;
+			}	
 		}
 	}
 }
